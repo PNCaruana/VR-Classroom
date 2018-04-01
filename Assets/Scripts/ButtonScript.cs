f5f783ec71433e9ef6e5350aba07d3971b06d475
unknown <myrealactualemail@gmail.com>
2018-03-31 21:16:15 -0400

    Unity 1st commit 9:15 PM 3/31

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+
+    public GameObject player;
+    public GameObject cam;
+    float v = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+   
+
+
+
+    // Update is called once per frame
+    void Update () {
+
+        
+       
+
+
+
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            transform.Translate(new Vector3(0, 5, 0));
+        }
+	}
+}
---
+++ Assets/Scripts/GuiScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 10, 10), "hello");
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
